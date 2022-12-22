cb07b41bfbdde41ea060c044f736db805600be72
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-22 13:50:10 +0400

    user class create

---
+++ essense/user/user.js
@@ -0,0 +1,15 @@
+import { Essense } from "../essense.js"
+
+export class User extends Essense {
+    constructor(){
+        // use superclass constructor
+        super()
+        // create singelton instance
+        if(User.instance){
+            // return instance
+            return User.instance
+        }
+        // create instance
+        User.instance = this
+    }
+}
