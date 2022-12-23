7a507abb60b12f5158d9bb8fd12873ff67e58b2d
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-23 07:52:39 +0400

    observer patter handler add

---
+++ game/game.js
@@ -0,0 +1,37 @@
+export class Game {
+    #gameLevel
+    #canvas
+    #ctx
+    constructor() {
+        if(Game.instance){
+            return Game.instance
+        }
+        Game.instance = this
+        this.#gameLevel = 0
+        this.#canvas = document.querySelector('.canvas')
+        this.#ctx = this.#canvas.getContext('2d')
+    }
+    #canvasSize = () => {
+        this.#canvas.style.width = window.innerWidth + 'px'
+        this.#canvas.style.height = window.innerHeight + 'px'
+        this.#canvas.width = window.innerWidth
+        this.#canvas.height = window.innerHeight
+    }
+    #clearCanvas = (x = 0, y = 0) => {
+        this.#ctx.clearRect(0, y, x, this.#canvas.height)
+    }
+    #animationFrame = () => {
+        this.#clearCanvas()
+        requestAnimationFrame(this.#animationFrame)
+    }
+    #eventsHandler = () => {
+        this.#canvasSize()
+        window.addEventListener('resize', this.#canvasSize)
+        requestAnimationFrame(this.#animationFrame)
+    }
+    StartGame = () => {
+        this.#eventsHandler()
+    }
+}
+
+
