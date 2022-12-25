ee57076b573f5ee45bff0998d70b248a18dd424f
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-25 06:01:05 +0300

    update map generato

--- game/canvas/canvas.js
+++ game/canvas/canvas.js
@@ -1,10 +1,13 @@
 import { Observer } from '../../utils/observer.js'
 import { PositionMenu } from '../menuHandler.js'
+import { Generator } from '../map/generator.js'
 
 export class Canvas {
     #canvas
     #ctx
     #observer
+    #nodes
+    #tileSize = 8
     constructor () {
         this.#observer = new Observer()
         this.#canvas = document.querySelector('.canvas')
@@ -37,7 +40,34 @@ export class Canvas {
     #deleteEventHandler = () => {
         window.removeEventListener('resize', this.#canvasSize)
     }
+    #drawTree(node){
+        this.#ctx.fillStyle = '#181818'
+        let a = 0
+        for(let h = 0; h < node.rows; h++ )
+          for(let w = 0; w < node.cols; w++ ){
+                let index = node.cols * h + w
+                let y = index / node.cols >> 0
+                let x = (index - y * node.cols)
+                if(node.map[index] == 1)
+                    this.#ctx.fillRect( x * this.#tileSize, y * this.#tileSize, this.#tileSize, this.#tileSize)
+                a++
+            }
+        console.log(node)
+        for (let i =  0; i < node.rooms.length; i++) {
+            this.#ctx.strokeStyle = 'hsla('+a * 32+', 50%, 80%, 1)'
+            this.#ctx.fillStyle = 'hsla('+a * 32+', 50%, 50%, 0.2)'
+            this.#ctx.beginPath()
+            // this.#ctx.rect(node.rooms[i].x * this.#tileSize, node.rooms[i].y * this.#tileSize, node.rooms[i].w* this.#tileSize, node.rooms[i].h * this.#tileSize );
+            // this.#ctx.fillRect((node.rooms[i].x * this.#tileSize), (node.rooms[i].y * this.#tileSize), node.rooms[i].w * this.#tileSize, node.rooms[i].h * this.#tileSize);
+            // this.#ctx.stroke();
+            // this.#ctx.fillStyle = '#181818';
+            // this.#ctx.fillText(node.rooms[i].x, node.rooms[i].x * this.#tileSize, node.rooms[i].y * this.#tileSize);
+            a++
+        }
+    }
     StartGame = () => {
         this.#observer.notlify()
+        this.#nodes = new Generator(0, 0, this.#canvas.width / this.#tileSize >> 0, this.#canvas.height / this.#tileSize >> 0, 5)
+        this.#drawTree(this.#nodes)        
     }
 }
