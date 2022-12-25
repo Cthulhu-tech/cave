49205d3aa29b7fa65fcb1a19b422109c13fcc19c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-12-25 06:41:42 +0300

    observer update

--- game/canvas/canvas.js
+++ game/canvas/canvas.js
@@ -1,23 +1,32 @@
 import { Observer } from '../../utils/observer.js'
 import { PositionMenu } from '../menuHandler.js'
-import { Generator } from '../map/generator.js'
-
+import { Map } from '../map/drawMap.js'
 export class Canvas {
-    #canvas
     #ctx
+    #map
+    #canvas
     #observer
-    #nodes
-    #tileSize = 8
     constructor () {
+        if(Canvas.instance){
+            return Canvas.instance
+        }
+        Canvas.instance = this
         this.#observer = new Observer()
         this.#canvas = document.querySelector('.canvas')
         this.#ctx = this.#canvas.getContext('2d')
         this.#subscribe()
     }
+    GetCanvas = () => {
+        return this.#canvas
+    }
+    GetContext = () => {
+        return this.#ctx
+    }
     #subscribe = () => {
         new PositionMenu().HiddenSection()
         this.#observer.subscribe(this.#deleteEventHandler)
         this.#observer.subscribe(this.#eventsHandler)
+        this.#observer.subscribe(this.#generateMap)
     }
     #canvasSize = () => {
         this.#canvas.style.width = window.innerWidth + 'px'
@@ -25,11 +34,15 @@ export class Canvas {
         this.#canvas.width = window.innerWidth
         this.#canvas.height = window.innerHeight
     }
-    #clearCanvas = (x = 0, y = 0) => {
-        this.#ctx.clearRect(0, y, x, this.#canvas.height)
+    #clearCanvas = () => {
+        this.#ctx.clearRect(0, 0, this.#canvas.width, this.#canvas.height)
+    }
+    #generateMap = () => {
+        this.#map = new Map()
     }
     #animationFrame = () => {
         this.#clearCanvas()
+        this.#map.UpdateMap(this.#map.GetNodes())
         requestAnimationFrame(this.#animationFrame)
     }
     #eventsHandler = () => {
@@ -40,34 +53,7 @@ export class Canvas {
     #deleteEventHandler = () => {
         window.removeEventListener('resize', this.#canvasSize)
     }
-    #drawTree(node){
-        this.#ctx.fillStyle = '#181818'
-        let a = 0
-        for(let h = 0; h < node.rows; h++ )
-          for(let w = 0; w < node.cols; w++ ){
-                let index = node.cols * h + w
-                let y = index / node.cols >> 0
-                let x = (index - y * node.cols)
-                if(node.map[index] == 1)
-                    this.#ctx.fillRect( x * this.#tileSize, y * this.#tileSize, this.#tileSize, this.#tileSize)
-                a++
-            }
-        console.log(node)
-        for (let i =  0; i < node.rooms.length; i++) {
-            this.#ctx.strokeStyle = 'hsla('+a * 32+', 50%, 80%, 1)'
-            this.#ctx.fillStyle = 'hsla('+a * 32+', 50%, 50%, 0.2)'
-            this.#ctx.beginPath()
-            // this.#ctx.rect(node.rooms[i].x * this.#tileSize, node.rooms[i].y * this.#tileSize, node.rooms[i].w* this.#tileSize, node.rooms[i].h * this.#tileSize );
-            // this.#ctx.fillRect((node.rooms[i].x * this.#tileSize), (node.rooms[i].y * this.#tileSize), node.rooms[i].w * this.#tileSize, node.rooms[i].h * this.#tileSize);
-            // this.#ctx.stroke();
-            // this.#ctx.fillStyle = '#181818';
-            // this.#ctx.fillText(node.rooms[i].x, node.rooms[i].x * this.#tileSize, node.rooms[i].y * this.#tileSize);
-            a++
-        }
-    }
-    StartGame = () => {
-        this.#observer.notlify()
-        this.#nodes = new Generator(0, 0, this.#canvas.width / this.#tileSize >> 0, this.#canvas.height / this.#tileSize >> 0, 5)
-        this.#drawTree(this.#nodes)        
+    CanvasStart = () => {
+        this.#observer.notlify()   
     }
 }
